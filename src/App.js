0a38bd2e9bd2edc9aeeaca1a9c6450dba5d2ff4c
JD Wilson <jaydee.wilson@gmail.com>
2021-06-11 11:59:28 +0200

    commit2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [resturants, setResturants] = useState()
   const [page, setPage] = useState(10)
   const [resturantPage, setResturantPage] = useState([])
+  const [genre, setGenre] = useState([])
+  const [genrefilterText, setGenreFilterText] = useState('All')
 
   const prev = () => {
     setPage(prev => prev - 10)
@@ -21,18 +23,50 @@ function App() {
       })
     })
       .then(response => response.json())
-      .then(data => setResturants(data))
+      .then(data => {
+        let genreArr = ['All']
+        for (let i = 0; i < data.length; i++) {
+          for (let value of data[i].genre.split(',')) {
+
+            if (genreArr.indexOf(value) === -1) {
+              genreArr.push(value)
+            }
+          }
+        }
+
+        setGenre(genreArr)
+        setResturants(data)
+
+      })
+    // eslint-disable-next-line 
   }, [])
 
   useEffect(() => {
-    if (resturants && page < resturants.length) {
-      let nextPage = []
-      for (let index = page - 10; index < page; index++) {
-        nextPage.push(resturants[index])
+    let num = 10
+
+    if (page < 0) setPage(10)
+    if (resturants && page - num < resturants.length && page > 0) {
+      if (page + 10 > resturants.length && resturants.length - page > 0) {
+
+        num = resturants.length - page
+
       }
-      setResturantPage(prev => prev = nextPage)
+      if (num > 0) {
+        let nextPage = []
+        for (let index = page - num; index < page; index++) {
+          nextPage.push(resturants[index])
+        }
+        setResturantPage(prev => prev = nextPage)
+
+      }
     }
-  }, [page, resturants])
+  }, [page, genrefilterText, resturants])
+
+  function handelChangeGenre(value) {
+    setGenreFilterText(prev => prev = genre[value])
+    setPage(10)
+  }
+
   if (resturants) {
 
     return (
@@ -44,17 +78,34 @@ function App() {
           <h1>Phn Num</h1>
           <h1>Genres</h1>
         </div>
-        { resturantPage.map(resturant =>
-          <div className='row' key={resturant.id}>
-            <p>{resturant.name}</p>
-            <p>{resturant.city}</p>
-            <p>{resturant.state}</p>
-            <p>{resturant.telephone}</p>
-            <p>{resturant.genre}</p>
+        {resturantPage.filter(resturant =>
+          genrefilterText !== 'All' ? resturant.genre.split(',').indexOf(genrefilterText) !== -1
+            :
+            true
+        )
+          .map(resturant =>
+            resturant &&
+            <div className='row' key={resturant.id}>
+
+              <p>{resturant.name}</p>
+              <p>{resturant.city}</p>
+              <p>{resturant.state}</p>
+              <p>{resturant.telephone}</p>
+              <p>{resturant.genre}</p>
+            </div>
+          )}
+        <div className='footer'>
+          <button onClick={prev} className='prev'> Prev </button>
+          <div className="filter" >
+            <span>  Filter by Genre </span>
+            <select name='genre' onChange={e => handelChangeGenre(e.target.value)}>
+              {genre.map((value, i) =>
+                <option key={value} value={i}>{value}</option>
+              )}
+            </select>
           </div>
-        )}
-        <button onClick={prev}> Prev </button>
-        <button onClick={next}>Next </button>
+          <button onClick={next} className='next'>Next </button>
+        </div>
       </div>
     );
   }
